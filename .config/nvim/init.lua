5a86ee976e0bb550cbd4e5e878766570998317b4
Jack Herby <JacekZiolek@proton.me>
2026-04-19 22:39:38 +0200

    feat(neovim): Fix typo.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,7 +12,7 @@ vim.g.maplocalleader = " "
 -- Set to true if you have a Nerd Font installed.
 vim.g.have_nerd_font = true
 
--- disable netrw at the very start of your init.lua.
+-- Disable netrw at the very start of your init.lua.
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
