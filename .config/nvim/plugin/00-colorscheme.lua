2db55f7ff975197b5bb9842307e0db17701e093b
Jack Herby <JacekZiolek@proton.me>
2026-05-01 10:26:28 +0200

    feat: Catppuccin all the way, baby!

--- .config/nvim/plugin/00-colorscheme.lua
+++ .config/nvim/plugin/00-colorscheme.lua
@@ -1,37 +1,24 @@
--- A port of gruvbox community theme to lua with treesitter and semantic highlights support.
+-- Catppuccin for Neovim
 
 local githubUrl = require("utils.github-url")
 local packAdd = require("utils.pack-add")
 packAdd({
-  githubUrl("ellisonleao/gruvbox.nvim"), -- https://github.com/ellisonleao/gruvbox.nvim
+  { src = githubUrl("catppuccin/nvim"), name = "catppuccin" }, -- https://github.com/catppuccin/nvim
 })
 
-require("gruvbox").setup({
-  contrast = "hard",
-  overrides = {
-    DiagnosticUnderlineError = {
-      sp = "#cc241d",
-      undercurl = false,
-      underline = true,
+require("catppuccin").setup({
+  flavour = "mocha",
+  integrations = {
+    fidget = true,
+    harpoon = true,
+    indent_blankline = {
+      colored_indent_levels = true,
+      enabled = true,
+      scope_color = "maroon",
     },
-    DiagnosticUnderlineWarn = {
-      sp = "#d79921",
-      undercurl = false,
-      underline = true,
-    },
-    DiagnosticUnderlineInfo = {
-      sp = "#458588",
-      undercurl = false,
-      underline = true,
-    },
-    DiagnosticUnderlineHint = {
-      sp = "#98971a",
-      undercurl = false,
-      underline = true,
-    },
+    mason = true,
+    which_key = true,
   },
-  transparent_mode = not vim.g.neovide,
 })
 
-vim.o.background = "dark" -- or "light" for light mode
-vim.cmd("colorscheme gruvbox")
+vim.cmd("colorscheme catppuccin-nvim")
