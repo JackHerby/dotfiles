962530741e80144cf6278961e972457aef24507a
Jack Herby <JacekZiolek@proton.me>
2026-05-05 19:50:04 +0200

    feat(neovim): Update Neovide terminal colors.

--- .config/nvim/plugin/00-colorscheme.lua
+++ .config/nvim/plugin/00-colorscheme.lua
@@ -25,21 +25,20 @@ vim.cmd("colorscheme catppuccin-nvim")
 
 -- Set terminal color scheme for Neovide.
 if vim.g.neovide then
-  vim.g.terminal_color_0 = "#11111b"
+  vim.g.terminal_color_0 = "#45475a"
   vim.g.terminal_color_1 = "#f38ba8"
   vim.g.terminal_color_2 = "#a6e3a1"
   vim.g.terminal_color_3 = "#f9e2af"
   vim.g.terminal_color_4 = "#89b4fa"
-  vim.g.terminal_color_5 = "#cba6f7"
+  vim.g.terminal_color_5 = "#f5c2e7"
   vim.g.terminal_color_6 = "#94e2d5"
-  vim.g.terminal_color_7 = "#cdd6f4"
-
-  vim.g.terminal_color_8 = "#313244"
-  vim.g.terminal_color_9 = "#f38ba8"
-  vim.g.terminal_color_10 = "#a6e3a1"
-  vim.g.terminal_color_11 = "#f9e2af"
-  vim.g.terminal_color_12 = "#89b4fa"
-  vim.g.terminal_color_13 = "#cba6f7"
-  vim.g.terminal_color_14 = "#94e2d5"
+  vim.g.terminal_color_7 = "#a6adc8"
+  vim.g.terminal_color_8 = "#585b70"
+  vim.g.terminal_color_9 = "#f37799"
+  vim.g.terminal_color_10 = "#89d88b"
+  vim.g.terminal_color_11 = "#ebd391"
+  vim.g.terminal_color_12 = "#74a8fc"
+  vim.g.terminal_color_13 = "#f2aede"
+  vim.g.terminal_color_14 = "#6bd7ca"
   vim.g.terminal_color_15 = "#bac2de"
 end
