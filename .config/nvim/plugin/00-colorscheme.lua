c6c0f933dab32da6d007e0c4ad238ab692dedea3
Jacek Ziółek <jziolek@media4u.pl>
2026-05-05 09:54:37 +0200

    feat(neovim): Set terminal color scheme for Neovide.

--- .config/nvim/plugin/00-colorscheme.lua
+++ .config/nvim/plugin/00-colorscheme.lua
@@ -23,3 +23,24 @@ require("catppuccin").setup({
 })
 
 vim.cmd("colorscheme catppuccin-nvim")
+
+-- Set terminal color scheme for Neovide.
+if vim.g.neovide then
+  vim.g.terminal_color_0 = "#11111b"
+  vim.g.terminal_color_1 = "#f38ba8"
+  vim.g.terminal_color_2 = "#a6e3a1"
+  vim.g.terminal_color_3 = "#f9e2af"
+  vim.g.terminal_color_4 = "#89b4fa"
+  vim.g.terminal_color_5 = "#cba6f7"
+  vim.g.terminal_color_6 = "#94e2d5"
+  vim.g.terminal_color_7 = "#cdd6f4"
+
+  vim.g.terminal_color_8 = "#313244"
+  vim.g.terminal_color_9 = "#f38ba8"
+  vim.g.terminal_color_10 = "#a6e3a1"
+  vim.g.terminal_color_11 = "#f9e2af"
+  vim.g.terminal_color_12 = "#89b4fa"
+  vim.g.terminal_color_13 = "#cba6f7"
+  vim.g.terminal_color_14 = "#94e2d5"
+  vim.g.terminal_color_15 = "#bac2de"
+end
