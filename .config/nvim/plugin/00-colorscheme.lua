288f18b022735e94157843a53f65844b4b3de466
Jack Herby <JacekZiolek@proton.me>
2026-05-08 23:53:22 +0200

    Revert "feat: Catppuccin all the way, baby!": I'm sorry gruvbox, I was wrong.
    This reverts commit 2db55f7ff975197b5bb9842307e0db17701e093b.

--- .config/nvim/plugin/00-colorscheme.lua
+++ .config/nvim/plugin/00-colorscheme.lua
@@ -1,44 +1,37 @@
--- Catppuccin for Neovim
+-- A port of gruvbox community theme to lua with treesitter and semantic highlights support.
 
 local githubUrl = require("utils.github-url")
 local packAdd = require("utils.pack-add")
 packAdd({
-  { src = githubUrl("catppuccin/nvim"), name = "catppuccin" }, -- https://github.com/catppuccin/nvim
+  githubUrl("ellisonleao/gruvbox.nvim"), -- https://github.com/ellisonleao/gruvbox.nvim
 })
 
-require("catppuccin").setup({
-  flavour = "mocha",
-  integrations = {
-    fidget = true,
-    harpoon = true,
-    indent_blankline = {
-      colored_indent_levels = true,
-      enabled = true,
-      scope_color = "maroon",
+require("gruvbox").setup({
+  contrast = "hard",
+  overrides = {
+    DiagnosticUnderlineError = {
+      sp = "#cc241d",
+      undercurl = false,
+      underline = true,
     },
-    mason = true,
-    which_key = true,
+    DiagnosticUnderlineWarn = {
+      sp = "#d79921",
+      undercurl = false,
+      underline = true,
+    },
+    DiagnosticUnderlineInfo = {
+      sp = "#458588",
+      undercurl = false,
+      underline = true,
+    },
+    DiagnosticUnderlineHint = {
+      sp = "#98971a",
+      undercurl = false,
+      underline = true,
+    },
   },
+  transparent_mode = not vim.g.neovide,
 })
 
-vim.cmd("colorscheme catppuccin-nvim")
-
--- Set terminal color scheme for Neovide.
-if vim.g.neovide then
-  vim.g.terminal_color_0 = "#45475a"
-  vim.g.terminal_color_1 = "#f38ba8"
-  vim.g.terminal_color_2 = "#a6e3a1"
-  vim.g.terminal_color_3 = "#f9e2af"
-  vim.g.terminal_color_4 = "#89b4fa"
-  vim.g.terminal_color_5 = "#f5c2e7"
-  vim.g.terminal_color_6 = "#94e2d5"
-  vim.g.terminal_color_7 = "#a6adc8"
-  vim.g.terminal_color_8 = "#585b70"
-  vim.g.terminal_color_9 = "#f37799"
-  vim.g.terminal_color_10 = "#89d88b"
-  vim.g.terminal_color_11 = "#ebd391"
-  vim.g.terminal_color_12 = "#74a8fc"
-  vim.g.terminal_color_13 = "#f2aede"
-  vim.g.terminal_color_14 = "#6bd7ca"
-  vim.g.terminal_color_15 = "#bac2de"
-end
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd("colorscheme gruvbox")
