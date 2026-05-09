8dba7a9a26bf7eb53030d80e5cf0be7368d1db50
Jack Herby <JacekZiolek@proton.me>
2026-05-09 08:41:22 +0200

    feat(neovim): Update nvim-tree keymaps.

--- .config/nvim/plugin/03-nvim-tree.lua
+++ .config/nvim/plugin/03-nvim-tree.lua
@@ -24,9 +24,13 @@ require("nvim-tree").setup({
 })
 
 local keymap = require("utils.keymap")
-keymap("<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "[c]ollapse nvim-tree recursively" })
-keymap("<leader>ef", "<cmd>NvimTreeFindFile<CR>", { desc = "move cursor to current [f]ile in Nvim Tree" })
-keymap("<leader>eg", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "find file or to[g]gle Nvim Tree" })
-keymap("<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "[r]efresh Nvim Tree" })
-keymap("<leader>es", "<cmd>NvimTreeFocus<CR>", { desc = "Nvim Tree focu[s]" })
-keymap("<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "[t]oggle Nvim Tree" })
+keymap("<leader>ec", function() vim.cmd("NvimTreeCollapse") end, { desc = "[c]ollapse nvim-tree recursively" })
+keymap("<leader>ee", function() vim.cmd("NvimTreeToggle") end, { desc = "[t]oggle Nvim Tree" })
+keymap(
+  "<leader>ef",
+  function() vim.cmd("NvimTreeFindFile") end,
+  { desc = "move cursor to current [f]ile in Nvim Tree" }
+)
+keymap("<leader>eg", function() vim.cmd("NvimTreeFindFileToggle") end, { desc = "find file or to[g]gle Nvim Tree" })
+keymap("<leader>er", function() vim.cmd("NvimTreeRefresh") end, { desc = "[r]efresh Nvim Tree" })
+keymap("<leader>es", function() vim.cmd("NvimTreeFocus") end, { desc = "Nvim Tree focu[s]" })
