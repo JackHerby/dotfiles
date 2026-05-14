edaae1cde55a73f775ea1d9dbae0d8126c782687
Jacek Ziółek <jziolek@media4u.pl>
2026-05-14 22:57:30 +0200

    Revert "feat(neovim): Change mind yet again and remove nvim-tree plugin."
    This reverts commit 71396396adf85156625abde6f95fe082e4922823.

---
+++ .config/nvim/plugin/03-nvim-tree.lua
@@ -0,0 +1,36 @@
+-- A File Explorer For Neovim Written In Lua.
+
+local githubUrl = require("utils.github-url")
+local packAdd = require("utils.pack-add")
+packAdd({
+  githubUrl("nvim-tree/nvim-tree.lua"), -- https://github.com/nvim-tree/nvim-tree.lua
+})
+
+require("nvim-tree").setup({
+  actions = {
+    open_file = {
+      window_picker = {
+        enable = false,
+      },
+    },
+  },
+  disable_netrw = true,
+  view = {
+    width = function() return math.floor(vim.o.columns * 0.33) end,
+    side = "right",
+    number = true,
+    cursorline = true,
+  },
+})
+
+local keymap = require("utils.keymap")
+keymap("<leader>tc", function() vim.cmd("NvimTreeCollapse") end, { desc = "[c]ollapse nvim-tree recursively" })
+keymap("<leader>te", function() vim.cmd("NvimTreeToggle") end, { desc = "[t]oggle Nvim Tree" })
+keymap(
+  "<leader>tf",
+  function() vim.cmd("NvimTreeFindFile") end,
+  { desc = "move cursor to current [f]ile in Nvim Tree" }
+)
+keymap("<leader>tg", function() vim.cmd("NvimTreeFindFileToggle") end, { desc = "find file or to[g]gle Nvim Tree" })
+keymap("<leader>tr", function() vim.cmd("NvimTreeRefresh") end, { desc = "[r]efresh Nvim Tree" })
+keymap("<leader>ts", function() vim.cmd("NvimTreeFocus") end, { desc = "Nvim Tree focu[s]" })
