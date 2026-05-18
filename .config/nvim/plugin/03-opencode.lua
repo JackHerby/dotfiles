1ff850150e4a78217f9e4bdb8d5ef9ce2c52fbe1
Jacek Ziółek <jziolek@media4u.pl>
2026-05-18 10:18:18 +0200

    Revert "feat(opencode) Remove plugin."
    This reverts commit 65a53494cab7bc4aa206e2225973931c4b258acb.

---
+++ .config/nvim/plugin/03-opencode.lua
@@ -0,0 +1,64 @@
+-- Opencode integration for Neovim.
+
+local githubUrl = require("utils.github-url")
+local packAdd = require("utils.pack-add")
+packAdd({
+  githubUrl("NickvanDyke/opencode.nvim"), -- https://github.com/NickvanDyke/opencode.nvim
+})
+
+---@type opencode.Opts
+vim.g.opencode_opts = {
+  server = {
+    start = false,
+    stop = false,
+    toggle = false,
+  },
+}
+
+-- Required for `opts.events.reload`.
+vim.o.autoread = true
+
+local keymap = require("utils.keymap")
+keymap(
+  "<C-a>",
+  function() require("opencode").command("session.half.page.up") end,
+  { desc = "Half page up." },
+  { "n", "x" }
+)
+keymap(
+  "<C-x>",
+  function() require("opencode").command("session.half.page.down") end,
+  { desc = "Half page down." },
+  { "n", "x" }
+)
+keymap(
+  "<leader>oa",
+  function() require("opencode").ask("", { submit = true }) end,
+  { desc = "[A]sk Opencode." },
+  { "n", "v", "x" }
+)
+keymap(
+  "<leader>oc",
+  function() require("opencode").ask("@this: ", { submit = true }) end,
+  { desc = "opencode ask with [c]ontext." },
+  { "n", "v", "x" }
+)
+keymap(
+  "<leader>ol",
+  function() return require("opencode").operator("@this ") .. "_" end,
+  { desc = "Add [l]ine to Opencode.", expr = true }
+)
+keymap(
+  "<leader>or",
+  function() return require("opencode").operator("@this ") end,
+  { desc = "Add [r]ange to Opencode.", expr = true },
+  { "n", "x" }
+)
+keymap(
+  "<leader>os",
+  function() require("opencode").select() end,
+  { desc = "Execute Opencode [s]elected action." },
+  { "n", "v", "x" }
+)
+keymap("+", "<C-a>", { desc = "increment", noremap = true })
+keymap("-", "<C-x>", { desc = "decrement", noremap = true })
