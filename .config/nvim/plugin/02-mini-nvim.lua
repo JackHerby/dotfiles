78e1ed1625817951efcf4253d793954a026406be
Jacek Ziółek <jziolek@media4u.pl>
2026-07-15 15:35:58 +0200

    feat(neovim): Refactor mini.nvim config and group mini.nvim keymaps.

--- .config/nvim/plugin/02-mini-nvim.lua
+++ .config/nvim/plugin/02-mini-nvim.lua
@@ -2,38 +2,26 @@
 
 local githubUrl = require('utils.github-url')
 local packAdd = require('utils.pack-add')
+local keymap = require('utils.keymap')
+
 packAdd({
   githubUrl('echasnovski/mini.nvim'), -- https://github.com/echasnovski/mini.nvim
 })
 
 require('mini.ai').setup({ n_lines = 500 })
-
-local miniBufremove = require('mini.bufremove')
-miniBufremove.setup()
-local keymap = require('utils.keymap')
-keymap('<leader>bd', miniBufremove.delete, { desc = '[D]elete buffer.' })
-keymap('<leader>bu', miniBufremove.unshow, { desc = '[U]nshow buffer.' })
-keymap('<leader>bw', miniBufremove.wipeout, { desc = '[W]ipeout buffer.' })
-
-require('mini.diff').setup({
-  view = {
-    style = 'sign',
-  },
-})
-
+require('mini.bufremove').setup()
+require('mini.diff').setup({ view = { style = 'sign' } })
 require('mini.indentscope').setup()
-
 require('mini.move').setup()
-
-local miniStatusline = require('mini.statusline')
-miniStatusline.setup({
-  use_icons = vim.g.have_nerd_font,
-})
+require('mini.statusline').setup({ use_icons = vim.g.have_nerd_font, })
 ---@diagnostic disable-next-line: duplicate-set-field
-miniStatusline.section_location = function() return '%2l:%-2v' end
-
+MiniStatusline.section_location = function() return '%2l:%-2v' end
 require('mini.surround').setup()
-
 require('mini.tabline').setup()
-
 require('mini.trailspace').setup()
+
+keymap('<leader>md', MiniBufremove.delete, { desc = '[D]elete buffer.' })
+keymap('<leader>ml', MiniTrailspace.trim_last_lines, { desc = 'Trim [l]ast lines.' })
+keymap('<leader>mt', MiniTrailspace.trim, { desc = '[T]rim.' })
+keymap('<leader>mu', MiniBufremove.unshow, { desc = '[U]nshow buffer.' })
+keymap('<leader>mw', MiniBufremove.wipeout, { desc = '[W]ipeout buffer.' })
