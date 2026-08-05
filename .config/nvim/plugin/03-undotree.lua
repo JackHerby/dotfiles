712252052ce85a1a9e46e8cce2440bb9e2ef3388
Jacek Ziółek <jziolek@media4u.pl>
2026-08-05 14:30:49 +0200

    feat(neovim): Add quit keymap, add Fugitive and Undotree keymaps, remove yazi.nvim and add nvim-tree.

--- .config/nvim/plugin/03-undotree.lua
+++ .config/nvim/plugin/03-undotree.lua
@@ -9,3 +9,10 @@ packAdd({
 vim.g.undotree_WindowLayout = 4
 vim.g.undotree_SplitWidth = 50
 vim.g.undotree_DiffpanelHeight = 20
+
+local keymap = require('utils.keymap')
+keymap('<leader>uf', function() vim.cmd('UndotreeFocus') end, { desc = '[F]ocus Undotree' })
+keymap('<leader>uh', function() vim.cmd('UndotreeHide') end, { desc = '[H]ide Undotree' })
+keymap('<leader>up', function() vim.cmd('UndotreePersistUndo') end, { desc = 'Undotree [p]ersist undo' })
+keymap('<leader>us', function() vim.cmd('UndotreeShow') end, { desc = '[S]how Undotree' })
+keymap('<leader>uu', function() vim.cmd('UndotreeToggle') end, { desc = 'Toggle [U]ndotree' })
