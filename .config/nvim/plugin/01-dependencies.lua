e194412d5c18f1bd0aa8b87ed0fc4306c796f13f
Jacek Ziółek <jziolek@media4u.pl>
2026-08-12 12:20:14 +0200

    feat(neovim): Remove Nvim Tree and reinstall Yazi plugin.

--- .config/nvim/plugin/01-dependencies.lua
+++ .config/nvim/plugin/01-dependencies.lua
@@ -1,7 +1,6 @@
 local githubUrl = require('utils.github-url')
 local packAdd = require('utils.pack-add')
 packAdd({
-  githubUrl('nvim-tree/nvim-web-devicons'), -- https://github.com/nvim-tree/nvim-web-devicons
   githubUrl('nvim-lua/plenary.nvim'), -- https://github.com/nvim-lua/plenary.nvim
   githubUrl('Bilal2453/luvit-meta'), -- https://github.com/Bilal2453/luvit-meta
 })
