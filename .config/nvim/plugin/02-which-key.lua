27b2b539ae5448ae0f4e9fc8c72f51294f52570a
Jacek Ziółek <jziolek@media4u.pl>
2026-08-21 07:05:50 +0200

    feat(neovim): Add write keymap.

--- .config/nvim/plugin/02-which-key.lua
+++ .config/nvim/plugin/02-which-key.lua
@@ -10,7 +10,6 @@ require('which-key').setup({
   -- Delay between pressing a key and opening which-key (milliseconds).
   delay = 0,
   icons = { mappings = vim.g.have_nerd_font },
-
   -- Document existing key chains.
   spec = {
     { '<leader>a', group = 'LSP [a]ctions' },
